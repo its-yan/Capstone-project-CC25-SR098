5101b7e0a2a5fb9fb3a86a64f3ba5ac66aef1000
its-yan <k2023_3232@sttheresia-jkt.sch.id>
2025-03-15 23:04:36 +0700

    The First creation

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,10 @@
+
+// Function sidenav OPEN
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+}
+// Function sidenav CLOSE
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
+// END
