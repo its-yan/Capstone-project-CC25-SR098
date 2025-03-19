295a7e54c47f376e68ae343e0bed9b5c87048874
its-yan <k2023_3232@sttheresia-jkt.sch.id>
2025-03-19 08:47:10 +0700

    Navbar Fix

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-
-// Function sidenav OPEN
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
 }
-// Function sidenav CLOSE
+
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
 }
-// END
